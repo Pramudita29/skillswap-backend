04a3b28fb477168f07a403a8b09586f6cab0ffe6
Pramudita <pbhattarai0129@gmail.com>
2025-07-26 20:22:23 +0545

    add message model with schema for storing messages between users

---
+++ models/Message.js
@@ -0,0 +1,14 @@
+// models/Message.js
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema(
+  {
+    from: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    to: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    text: { type: String, required: true },
+    time: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Message', messageSchema);
